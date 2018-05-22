b35898811fd186829000be615539d3bf0249ffd0
ИгнатьевС <@238_9>
2018-05-22 10:00:45 +0600

    Add project files.

---
+++ ConsoleApplication1/Source.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void main()
+{
+
+
+
+
+	int x = 0, y;
+	printf("введите значение x: ");
+	scanf("%d", &x)
+		y = 7x2 - 3x + 6
+
+		printf("значение функции %d\n" y);
+
+
+
+}
